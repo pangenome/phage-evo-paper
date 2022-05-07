248dc82f633aa3ae42338b6f58441565153ce278
Hugo Avila <99145795+bioinformagica@users.noreply.github.com>
2022-05-07 18:55:56 -0300

    Added R_env.yaml

--- workflow/scripts/phylogeny.R
+++ workflow/scripts/phylogeny.R
@@ -13,35 +13,18 @@ suppressPackageStartupMessages(
 input <- '/home/hugo/projects/phage-evo-paper/results/single/pggb/distance_matrix_removed_ecoli_removed_phages.tsv'
 output <- 'plots'
 
-# colfunc <- colorRampPalette(c("red", "yellow"))
-# phage.colors=c(colfunc(10), rainbow(8)[3:7])
-
+colfunc <- colorRampPalette(c("red", "yellow"))
+phage.groups <- c('S2-55s', paste0('P',1:10))
+phage.colors <- c('#faa0f4', colfunc(10))
+names(phage.colors) <- phage.groups
+  
 y <- read.delim(input)
 y %>% mutate(path.a=path.a, path.b=path.b, jaccard=jaccard, path.a.length=NULL, path.b.length=NULL, intersection=NULL, euclidean=NULL) %>% pivot_wider(names_from=path.b, values_from=jaccard) %>% replace(is.na(.), 0) -> y.dist
 y.tree <- nj(as.dist(y.dist[, !names(y.dist) %in% c("path.a")]))
 
-p <- ggtree(y.tree, layout = 'rectangular', branch.length = 'none') %<+% data_frame(
-  labels = y.tree$tip.label,
-  passages = str_split_fixed(y.tree$tip.label, "#", 2)[,1]
-) + aes(color=passages)  + geom_tree()
-
-# Eriks code
-# ggtree(y.tree) %<+% data.frame(
-#   node=1:nrow(y.tree$edge),
-#   group.name=factor()
-# ) + aes(color=group.name)
-#   + geom_tree()
-#   + scale_color_manual("passage",values=c(phage.colors, "black"))
-
-# p <- ggtree(y.tree) %<+% data.frame(
-#   node=1:nrow(y.tree$edge),
-#   group.name=factor(
-#     c(as.character(y$path.a),rep("internal",nrow(y.tree$edge) - nrow(y))),
-#     levels=c(levels(y$path.a),"internal") 
-#   )
-# )
-#   + aes(color=group.name)
-#   + geom_tree()
-#   + scale_color_manual("passage",values=c(phage.colors, "black"))
+group.info <- split(y.tree$tip.label, gsub("#.+", "", y.tree$tip.label))
+y.tree <- groupOTU(y.tree, group.info)
+p <- ggtree(y.tree, branch.length = 'none', layout = 'daylight') + geom_tippoint(aes(color=group), size=2) + scale_color_manual('Passages' , values=phage.colors)
+dev.off()
 
 # ggsave(paste(output, "ggtree.passage.pdf", sep="."), height=40, width=9)
